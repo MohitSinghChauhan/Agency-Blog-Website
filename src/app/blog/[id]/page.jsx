863d5743a4c804655ff76583ed64402bcb070ec0
Mohit Singh Chauhan <mohit.chauhan2580@gmail.com>
2023-07-25 06:26:39 +0530

    feat: implemented data from placeholder APIs

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,44 +1,80 @@
-import React from "react";
-import styles from "./page.module.css";
-import Image from "next/image";
+import React from 'react';
+import styles from './page.module.css';
+import Image from 'next/image';
+import {notFound} from 'next/navigation'
+
+async function getBlogData(id) {
+	const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+		next: { revalidate: 10 },
+	});
+
+	if (!res.ok) {
+		return notFound();
+	}
+
+	return res.json();
+}
 
 const BlogPost = async ({ params }) => {
+	const blog = await getBlogData(params.id);
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.top}>
-        <div className={styles.info}>
-          <h1 className={styles.title}>title</h1>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, explicabo laudantium quasi cupiditate atque expedita dolores qui, at provident deleniti nesciunt amet perspiciatis numquam modi perferendis fuga distinctio reprehenderit mollitia nam inventore! Nihil facere eligendi voluptatum praesentium, magnam dolorem illo.
-          </p>
-          <div className={styles.author}>
-            <Image
-              src="https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-              alt=""
-              width={40}
-              height={40}
-              className={styles.avatar}
-            />
-            <span className={styles.username}>username</span>
-          </div>
-        </div>
-        <div className={styles.imageContainer}>
-          <Image
-            src="https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-            fill={true}
-            className={styles.image}
-          />
-        </div>
-      </div>
-      <div className={styles.content}>
-        <p className={styles.text}>
-         Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti hic rerum adipisci harum quae, accusantium praesentium consequuntur reprehenderit, sit accusamus ullam! Maiores nostrum illum earum dicta repellat doloremque, minima harum quia qui iste dolorem fugiat, explicabo et velit dignissimos eum? Explicabo sapiente nulla temporibus eligendi minus quis nihil, culpa hic neque minima quam, vel corporis voluptatibus enim. Praesentium suscipit architecto commodi magnam ratione natus laudantium? Non fugiat facilis asperiores quaerat eius fuga sequi? Aliquam, in quis, voluptas eos dicta repellendus vitae hic consectetur quidem alias rerum sit cum cupiditate quo quasi dolorum? Excepturi architecto aliquid mollitia et optio exercitationem error rem earum sit facere, molestiae blanditiis. Ullam corporis eligendi culpa enim unde itaque repellat est ipsum cupiditate voluptatum praesentium voluptas, sapiente voluptatem perferendis excepturi quaerat cumque aliquam libero veniam amet, sit nesciunt sint rerum dignissimos? Distinctio, ipsum. Ullam ea excepturi fugit eius expedita repellat repellendus assumenda, sed illo harum nam eaque odit commodi amet velit quisquam accusantium? Optio rerum nemo, expedita, magni ut sit, ducimus velit voluptatibus maiores cumque corporis illo excepturi? Numquam, repellat reprehenderit. Omnis accusamus praesentium assumenda, voluptas ab, doloribus velit quibusdam odio nostrum itaque quo, suscipit recusandae? Dignissimos quisquam harum expedita quam dolore debitis, ex doloribus rerum!
-        </p>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<div className={styles.top}>
+				<div className={styles.info}>
+					<h1 className={styles.title}>{blog.title}</h1>
+					<p className={styles.desc}>
+						{blog.body}
+					</p>
+					<div className={styles.author}>
+						<Image
+							src='https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+							alt=''
+							width={40}
+							height={40}
+							className={styles.avatar}
+						/>
+						<span className={styles.username}>username</span>
+					</div>
+				</div>
+				<div className={styles.imageContainer}>
+					<Image
+						src='https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+						alt=''
+						fill={true}
+						className={styles.image}
+					/>
+				</div>
+			</div>
+			<div className={styles.content}>
+				<p className={styles.text}>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti hic
+					rerum adipisci harum quae, accusantium praesentium consequuntur
+					reprehenderit, sit accusamus ullam! Maiores nostrum illum earum dicta
+					repellat doloremque, minima harum quia qui iste dolorem fugiat,
+					explicabo et velit dignissimos eum? Explicabo sapiente nulla
+					temporibus eligendi minus quis nihil, culpa hic neque minima quam, vel
+					corporis voluptatibus enim. Praesentium suscipit architecto commodi
+					magnam ratione natus laudantium? Non fugiat facilis asperiores quaerat
+					eius fuga sequi? Aliquam, in quis, voluptas eos dicta repellendus
+					vitae hic consectetur quidem alias rerum sit cum cupiditate quo quasi
+					dolorum? Excepturi architecto aliquid mollitia et optio exercitationem
+					error rem earum sit facere, molestiae blanditiis. Ullam corporis
+					eligendi culpa enim unde itaque repellat est ipsum cupiditate
+					voluptatum praesentium voluptas, sapiente voluptatem perferendis
+					excepturi quaerat cumque aliquam libero veniam amet, sit nesciunt sint
+					rerum dignissimos? Distinctio, ipsum. Ullam ea excepturi fugit eius
+					expedita repellat repellendus assumenda, sed illo harum nam eaque odit
+					commodi amet velit quisquam accusantium? Optio rerum nemo, expedita,
+					magni ut sit, ducimus velit voluptatibus maiores cumque corporis illo
+					excepturi? Numquam, repellat reprehenderit. Omnis accusamus
+					praesentium assumenda, voluptas ab, doloribus velit quibusdam odio
+					nostrum itaque quo, suscipit recusandae? Dignissimos quisquam harum
+					expedita quam dolore debitis, ex doloribus rerum!
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default BlogPost;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,64 +2,42 @@ import React from 'react';
 import styles from './page.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
+import { notFound } from 'next/navigation';
+
+async function getBlogsData() {
+	const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+		next: { revalidate: 10 },
+	});
+	if (!res.ok) {
+		return notFound();
+	}
+	return res.json();
+}
 
 const Blog = async () => {
+	const blogs = await getBlogsData();
 	return (
 		<div className={styles.mainContainer}>
-			<Link
-				href={`/blog/ok`}
-				className={styles.container}
-			>
-				<div className={styles.imageContainer}>
-					<Image
-						src='https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-						alt=''
-						width={400}
-						height={250}
-						className={styles.image}
-					/>
-				</div>
-				<div className={styles.content}>
-					<h1 className={styles.title}>Test</h1>
-					<p className={styles.desc}>Desc</p>
-				</div>
-			</Link>
-			<Link
-				href={`/blog/ok`}
-				className={styles.container}
-			>
-				<div className={styles.imageContainer}>
-					<Image
-						src='https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-						alt=''
-						width={400}
-						height={250}
-						className={styles.image}
-					/>
-				</div>
-				<div className={styles.content}>
-					<h1 className={styles.title}>Test</h1>
-					<p className={styles.desc}>Desc</p>
-				</div>
-			</Link>
-			<Link
-				href={`/blog/ok`}
-				className={styles.container}
-			>
-				<div className={styles.imageContainer}>
-					<Image
-						src='https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-						alt=''
-						width={400}
-						height={250}
-						className={styles.image}
-					/>
-				</div>
-				<div className={styles.content}>
-					<h1 className={styles.title}>Test</h1>
-					<p className={styles.desc}>Desc</p>
-				</div>
-			</Link>
+			{blogs.map((blog) => (
+				<Link
+					href={`/blog/${blog.id}`}
+					className={styles.container}
+					key={blog.id}>
+					<div className={styles.imageContainer}>
+						<Image
+							src='https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+							alt=''
+							width={400}
+							height={250}
+							className={styles.image}
+						/>
+					</div>
+					<div className={styles.content}>
+						<h1 className={styles.title}>{blog.title}</h1>
+						<p className={styles.desc}>{blog.body}</p>
+					</div>
+				</Link>
+			))}
 		</div>
 	);
 };
