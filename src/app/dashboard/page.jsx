46dc1394a1a3fb2c0b59e995abd2cb763509d4f3
Mohit Singh Chauhan <mohit.chauhan2580@gmail.com>
2023-07-25 08:13:20 +0530

    feat: implemented client side data fetching- useEffect

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+'use client';
+import { useEffect, useState } from 'react';
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+	const [data, setData] = useState([]);
+	const [err, setErr] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
 
-export default Dashboard
+  useEffect(()=>{
+    const getBlogData = async function getBlogData() {
+      setIsLoading(true);
+      const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`, {
+        next: { revalidate: 10 },
+      });
+    
+      if (!res.ok) {
+        return notFound();
+      }
+    
+      const data = await res.json();
+      setData(data);
+    }
+    getBlogData();
+    setIsLoading(false);
+  },[])
+
+	return <div>Dashboard</div>;
+};
+
+export default Dashboard;
