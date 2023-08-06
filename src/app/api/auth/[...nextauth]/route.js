4d78644b2396e1243c905e3428c50e71a34d0cec
Mohit Singh Chauhan <mohit.chauhan2580@gmail.com>
2023-08-07 03:51:12 +0530

    feat: Implementented User Login

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,50 @@
+import UserModel from '@/models/UserModel';
+import connect from '@/utils/db';
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import bcrypt from 'bcryptjs';
+
+const handler = NextAuth({
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+		}),
+		CredentialsProvider({
+			name: 'Credentials',
+			id: 'credentials',
+			async authorize(credentials) {
+				try {
+					await connect();
+
+					//Check if user exists
+					const user = await UserModel.findOne({
+						email: credentials.email,
+					});
+					if (user) {
+						const isPasswordMatch = await bcrypt.compare(
+							credentials.password,
+							user.password
+						);
+						console.log(isPasswordMatch);
+						if (isPasswordMatch) {
+							return user;
+						} else {
+							throw new Error('Invalid Credentials');
+						}
+					} else {
+						throw new Error('User does not exist');
+					}
+				} catch (err) {
+					throw new Error(err);
+				}
+			},
+		}),
+	],
+	pages: {
+		error: '/dashboard/login',
+	},
+});
+
+export { handler as GET, handler as POST };
--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+'use client';
+
+import { signIn } from 'next-auth/react';
+import styles from './page.module.css';
+// import { useRouter } from 'next/navigation';
+// import { useState } from 'react';
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+	// const [error, setError] = useState(null);
 
-export default Login
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const email = e.target[0].value;
+		const password = e.target[1].value;
+
+		signIn('credentials', {email,password});
+	};
+
+	return (
+		<div className={styles.container}>
+			<h1 className={styles.title}>Login to Your Account</h1>
+			<h2 className={styles.subtitle}>
+				Please login to see the dashboard.
+			</h2>
+			<form onSubmit={handleSubmit} className={styles.form}>
+				<input
+					type='text'
+					placeholder='Email'
+					required
+					className={styles.input}
+				/>
+				<input
+					type='password'
+					placeholder='Password'
+					required
+					className={styles.input}
+				/>
+				<button className={styles.button}>Login</button>
+				{/* {error && 'Something went wrong!'} */}
+			</form>
+
+			<button className={styles.button} onClick={() => signIn('google')}>
+				Sign in with Google
+			</button>
+		</div>
+	);
+};
+
+export default Login;
